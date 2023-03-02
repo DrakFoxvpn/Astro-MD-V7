9de1db4df6a1debdce12a36e474ccbdc306ea728
DrakFoxvpn <124435953+DrakFoxvpn@users.noreply.github.com>
2023-03-02 21:13:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
-ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
+ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://imgur.com/0SqgH2x.jpg' : process.env.ALIVE_LOGO,
 FOOTER: process.env.FOOTER === undefined ? '🛡Fox-ᴍᴅ Bot🔮' : process.env.FOOTER,
 CAPTION: process.env.CAPTION === undefined ? '*🖥ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ FoxAlpha📿*' : process.env.CAPTION,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
-INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
+INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed👿' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
 SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
 };
